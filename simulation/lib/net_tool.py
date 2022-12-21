8f22b03340cb73e2d9f1c3145be04c5bc1a01b52
chavels07 <73649460+chavels07@users.noreply.github.com>
2022-12-21 14:08:35 +0800

    tmp

---
+++ simulation/lib/net_tool.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2022/12/21 12:44
+# @File        : net_tool.py
+# @Description :
+
+class JunctionConns:
+    pass
+
+def entry_movement_sorted():
+    pass
